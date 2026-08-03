5a5b473d10753820b792f222bdf1aa8294897025
414-02-pranrak <54182@yupparaj.ac.th>
2026-08-03 14:34:07 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,9 +1,10 @@
-import streamlit as st 
- st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+import streamlit as st
+st.title("🛒แอปพลิเคชันคำนวณราคาสินค้ารวม VAT 7%")
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0, min_value=0.0)
+# คำนวณ VAT และราคารวม
 vat = price * 0.07
-net_price = price - vat
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท"
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+net_price = price + vat
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): {vat:.2f} บาท")
+st.header(f"• ราคาสุทธิ (รวม VAT): {net_price:.2f} บาท")
 st.divider()
-st.write("นางสาวปราณรักษ์ เมืองมูล เลขที่ 2  ม.4/14")
+st.write("นางสาวปราณรักษ์ เมืองมูล เลขที่ 2 ม.4/14")
